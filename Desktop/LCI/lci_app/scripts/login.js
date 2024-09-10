e869d7f75ada6b995fe14a2796d7aeb36f3e45c1
Awal Massaoudou <124563602+professeur01@users.noreply.github.com>
2024-09-10 16:43:42 +0100

    Delete Desktop/LCI/lci_app directory

--- Desktop/LCI/lci_app/scripts/login.js
+++
@@ -1,5 +0,0 @@
-const submit = document.getElementById("submit");
-
-document.querySelector("form").addEventListener("submit", function (e) {
-  e.preventDefault(); // Empêche l'envoi du formulaire
-});
